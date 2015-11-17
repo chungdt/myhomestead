604478b709e3766e226824b45be9ea38c7cc8cd3
Duong Thanh Chung <thanhchung2006@gmail.com>
2015-11-17 09:36:29 +0700

    update file init.sh

--- init.sh
+++ init.sh
@@ -2,14 +2,14 @@
 
 mysqlRoot1=~/allproject/datamysql/mysql01
 mysqlRoot2=~/allproject/datamysql/mysql02
-hawaiioptionRoot=~/allproject/hawaiioption/public
-superstarRoot=~/allproject/superstar/public
+site1Root=~/allproject/site1/public
+site2Root=~/allproject/site2/public
 apiRoot=~/allproject/api/public
 
 mkdir -pv "$mysqlRoot1"
 mkdir -pv "$mysqlRoot2"
-mkdir -pv "$hawaiioptionRoot"
-mkdir -pv "$superstarRoot"
+mkdir -pv "$site1Root"
+mkdir -pv "$site2Root"
 mkdir -pv "$apiRoot"
 
 echo "Create directories successful!"
